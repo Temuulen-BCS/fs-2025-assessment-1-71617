76ed48f23399be56a67dfddd04eee2dd70644f15
Temka <Temka9043@gmail.com>
2025-11-27 09:32:11 +0000

    Commit

---
+++ fs-2025-assessment-1-71617/Services/StationQuery.cs
@@ -0,0 +1,19 @@
+﻿namespace fs_2025_assessment_1_71617.Services
+{
+    public class StationQueryOptions
+    {
+        public string? Status { get; set; }
+        public string? Q { get; set; }
+        public int? MinBikes { get; set; }
+        public string? Sort { get; set; }
+        public string? Dir { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+
+        public string GetCacheKey()
+        {
+            return $"{Status}|{Q}|{MinBikes}|{Sort}|{Dir}|{Page}|{PageSize}";
+        }
+    }
+}
+

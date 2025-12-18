86afee5dfa98f7a61aa991be7343da26fb9e2502
Temka <Temka9043@gmail.com>
2025-12-18 09:55:19 +0000

    submit

---
+++ BikeStation.UI/models/Station.cs
@@ -0,0 +1,24 @@
+﻿namespace BikeStation.UI.Models;
+
+public class GeoPosition
+{
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+}
+
+public class Station
+{
+    public int Number { get; set; }
+    public string Contract_Name { get; set; } = "dublin";
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+    public GeoPosition Position { get; set; } = new();
+
+    public int Bike_Stands { get; set; }
+    public int Available_Bike_Stands { get; set; }
+    public int Available_Bikes { get; set; }
+    public string Status { get; set; } = "OPEN";
+
+    public long Last_Update { get; set; }
+}
---
+++ BikeStation.UI/models/StationDtos.cs
@@ -0,0 +1,21 @@
+﻿namespace BikeStation.UI.Models;
+
+public class StationDto
+{
+    public int Number { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+
+    public int BikeStands { get; set; }
+    public int AvailableBikeStands { get; set; }
+    public int AvailableBikes { get; set; }
+    public string Status { get; set; } = string.Empty;
+
+    public double Occupancy { get; set; }
+
+    public DateTimeOffset LastUpdateUtc { get; set; }
+    public DateTimeOffset LastUpdateLocal { get; set; }
+}
